0f25a0ef84160b0b24abc2fa6a0a0ec06cc53156
Sergei-Bug <122543402+Sergei-Bug@users.noreply.github.com>
2023-07-25 22:00:14 +0300

    add 1 and 2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  wrapper: document.querySelector('.gallery'),
+};
+
+const gellaryItemsEl = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a></li>`;
+  })
+  .join('');
+
+refs.wrapper.insertAdjacentHTML('beforeend', gellaryItemsEl);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+refs.wrapper.addEventListener('click', onClickGellaryItem);
+
+function onClickGellaryItem(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(TIME_KEY, data.seconds);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem(TIME_KEY);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
